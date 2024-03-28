1458e5570fb5e36743e15abe98792c9dd3ad61be
ABHISHEK KRISHNA <abhishekkrishna@ABHISHEKs-MacBook-Air.local>
2024-03-28 18:23:12 +0530

    LINK- Linked employee to id page + get id spefic page

--- frontend/src/screens/EmployeeScreen.js
+++ frontend/src/screens/EmployeeScreen.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 
 const EmployeeScreen = () => {
     const {id} = useParams()
+    useEffect(()=>{
+        const fetchEmployee = async()=>{
+            try{
+                const {data} = await axios.get(`/api/employee/${id}`)
+                console.log(data);
+            }catch(e){
+                console.log(e);
+            }
+        }
+
+        fetchEmployee()
+    },[])
   return (
     <div>
       Employee {id}
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -2,16 +2,19 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
 import { employeesActions } from "../store/employees";
+import { Link } from "react-router-dom";
 const HomeScreen = () => {
   // const [employees, setEmployees] = useState([]);
-  const dispatch = useDispatch()
-  const employeesRedux = useSelector(state => state.employeesReducer.employees)
+  const dispatch = useDispatch();
+  const employeesRedux = useSelector(
+    (state) => state.employeesReducer.employees
+  );
   useEffect(() => {
     const fetchEmployees = async () => {
       const { data } = await axios.get("/api/employees");
       console.log(data, typeof data);
       // setEmployees(data);
-      dispatch(employeesActions.populateEmployees(data))
+      dispatch(employeesActions.populateEmployees(data));
     };
     fetchEmployees();
   }, []);
@@ -35,20 +38,25 @@ const HomeScreen = () => {
           </tr>)
           
         })} */}
-        {employeesRedux && employeesRedux.map((employee) => {
-          console.log(employee.id);
-          return (<tr>
-            <td>{employee.id} </td>
-            <td>{employee.name}</td>
-            <td>{employee.designation}</td>
-            <td>{employee.phone}</td>
-          </tr>)
-          
-        })}
+        {employeesRedux &&
+          employeesRedux.map((employee) => {
+            console.log(employee.id);
+            return (
+              <tr>
+                <td>{employee.id}</td>
+                <td>
+                  <Link to={`/employee/${employee.id}`}>
+                    <strong>{employee.name}</strong>
+                  </Link>
+                </td>
+                <td>{employee.designation}</td>
+                <td>{employee.phone}</td>
+              </tr>
+            );
+          })}
       </table>
-      
     </div>
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
